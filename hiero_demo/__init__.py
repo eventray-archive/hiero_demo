8da03819d81eabcb989e56a24fa1bceb5b62e36b
John Anderson <sontek@gmail.com>
2012-07-08 19:06:14 -0500

    use updated hiero

--- hiero_demo/__init__.py
+++ hiero_demo/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config     import Configurator
+from pyramid.session    import UnencryptedCookieSessionFactoryConfig
 from hem.interfaces     import IDBSession
 from hiero_demo.models  import DBSession
 from sqlalchemy         import engine_from_config
@@ -6,7 +7,9 @@ from sqlalchemy         import engine_from_config
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    config = Configurator(settings=settings)
+    session_factory = UnencryptedCookieSessionFactoryConfig('itsaseekreet')
+    config = Configurator(settings=settings, session_factory=session_factory)
+
 
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
@@ -17,6 +20,7 @@ def main(global_config, **settings):
     config.add_view('hiero_demo.views.favicon_view', route_name='favicon')
 
     #config.include('hiero', route_prefix='/blog')
+    config.include('horus', route_prefix='auth')
     config.include('hiero', route_prefix='blog')
 
     config.scan()
